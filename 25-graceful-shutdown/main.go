d536296464c5477ed1615e301c57366402f000de
Bozz <peem@liveup.cc>
2020-02-02 13:31:42 +0700

    [Add] Graceful shutdown

--- 25-graceful-shutdown/main.go
+++ 25-graceful-shutdown/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -9,16 +14,29 @@ func main() {
 	http.HandleFunc("/", index)
 
 	// create http.Server
+	server := &http.Server{
+		Addr: ":3333",
+	}
 
 	// start server on another goroutine
+	go server.ListenAndServe()
 
 	// create buffered (size=1) channel for os.Signal
+	stop := make(chan os.Signal, 1)
 
 	// call signal.Notify to notify channel when received syscall.SIGTERM
+	signal.Notify(stop, syscall.SIGTERM, os.Interrupt)
 
 	// block until receive signal
+	<-stop
+	log.Println("Receive signal")
 
 	// shutdown server
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, 5*time.Minute)
+
+	server.Shutdown(ctx)
+	log.Println("Server already shutdown")
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
